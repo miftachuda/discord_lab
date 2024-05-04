606452e9c59dd26d4592228e5fa214e0de0ab58a
Miftachul <miftachulcc@gmail.com>
2024-05-04 22:32:29 +0700

    init

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits, Events } = require("discord.js");
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.MessageContent,
-  ],
-});
-client.login(process.env.CLIENT_TOKEN);
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const labware = require("./labware.js")
+const schedule = require('node-schedule');
+const axios = require('axios');
+
+class Discord {
+  async reply(text) {
+    const url = 'http://http.miftachuda.my.id';
+    const data = {
+      content: text,
+    };
+
+    try {
+      const response = await axios.post(url, data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      console.log('Data:', response.data);
+    } catch (error) {
+      console.error('Error:', error.response ? error.response.data : error.message);
+    }
+  }
+}
+
+const discord = new Discord();
+
+
+
+const times = [1, 10, 17, 22]; // 24-hour format times
+
+times.forEach(time => {
+  schedule.scheduleJob({ hour: time, minute: 33 }, function () {
+    discord.reply(`Executing task scheduled at ${time}:00`);
+    labware(discord)
+    discord.reply("Task running at:", new Date().toLocaleString());
+  });
 });
 
-client.channels.client.on(Events.MessageCreate, (msg) => {
-  if (msg.content == "lab") {
-    msg.reply(`Accessing Labware and getting data,\nPlease wait ... !!`);
-  }
-});
